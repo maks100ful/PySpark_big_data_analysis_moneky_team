9ebb0b3f47f4bfa6757f33c4570476bb56eb3787
alternmill <vitalik0969870505@gmail.com>
2025-04-13 11:05:04 +0000

    Fix schema + read all data to test schemas

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from pyspark.sql import SparkSession
-from shemas import title_basics_schema
+from shemas import *
 from pyspark.sql.functions import col
 
 def main():
     spark = SparkSession.builder.appName("MySparkApp").getOrCreate()
 
-    df = spark.read.csv(
+    df_title_basics = spark.read.csv(
     "raw_data/title.basics.tsv.gz",
     sep="\t",
     header=True,
@@ -14,7 +14,71 @@ def main():
     )
     # df = df.withColumn("startYear", col("startYear").cast("int"))
 
-    df.show(n = 50)
+    df_title_basics.show(n = 10)
+
+    df_name_basics = spark.read.csv(
+    "raw_data/name.basics.tsv.gz",
+    sep="\t",
+    header=True,
+    schema=name_basics_schema,
+    nullValue="\\N"
+    )
+
+    df_name_basics.show(n = 10)
+
+    df_title_akas = spark.read.csv(
+    "raw_data/title.akas.tsv.gz",
+    sep="\t",
+    header=True,
+    schema=title_akas_schema,
+    nullValue="\\N"
+    )
+
+    df_title_akas.show(n = 10)
+
+
+    df_title_crew = spark.read.csv(
+    "raw_data/title.crew.tsv.gz",
+    sep="\t",
+    header=True,
+    schema=title_crew_schema,
+    nullValue="\\N"
+    )
+
+    df_title_crew.show(n = 10)
+
+
+    df_title_episode = spark.read.csv(
+    "raw_data/title.episode.tsv.gz",
+    sep="\t",
+    header=True,
+    schema=title_episode_schema,
+    nullValue="\\N"
+    )
+
+    df_title_episode.show(n = 10)
+
+
+    df_title_principals = spark.read.csv(
+    "raw_data/title.principals.tsv.gz",
+    sep="\t",
+    header=True,
+    schema=title_principals_schema,
+    nullValue="\\N"
+    )
+
+    df_title_principals.show(n = 10)
+
+
+    df_title_ratings = spark.read.csv(
+    "raw_data/title.ratings.tsv.gz",
+    sep="\t",
+    header=True,
+    schema=title_ratings_schema,
+    nullValue="\\N"
+    )
+
+    df_title_ratings.show(n = 10)
 
     spark.stop()
 
