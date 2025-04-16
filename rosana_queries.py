b6c6b809f80fea3c67024605f8e308a7efbb6b37
Rosana <annros2003@gmail.com>
2025-04-16 06:38:28 +0000

    first two queries

--- rosana_queries.py
+++ rosana_queries.py
@@ -1,6 +1,21 @@
-from pyspark.sql import SparkSession, DataFrame
-from pyspark.sql.types import StructType
-from pyspark.sql.functions import col, sum
-from shemas import *
-import os
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import col, sum, avg
+from pyspark.sql.window import Window
+
+def get_top_rated_movies_in_2024_with_more_than_1000_votes(df_title_basic: DataFrame, df_title_ratings: DataFrame) -> DataFrame:
+    result = df_title_basic.join(df_title_ratings, on='tconst') \
+            .filter((col('startYear') == 2024) & (col('numVotes') > 1000)) \
+            .orderBy(col('averageRating').desc())
+    
+    return result
+
+def get_average_num_of_votes_by_language(df_title_akas: DataFrame, df_title_ratings: DataFrame) -> DataFrame:
+    result = df_title_akas.join(df_title_ratings, df_title_akas.titleId == df_title_ratings.tconst) \
+            .groupBy(col('language')) \
+            .agg(avg('numVotes').alias('average number of votes')) \
+            .orderBy(col('average number of votes').desc())
+    
+    return result
+    
+
 
