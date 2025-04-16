3bb43768d6b1da284f11832d9ee97e48e24ebaf7
maks100ful <mfil040503@gmail.com>
2025-04-16 18:59:29 +0000

    add querries

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dataframes import *
 from queries_vitalii import genre_combination_popilarity
 from rosana_queries import get_3_top_rated_alive_principals_by_category
 from maks_querries import get_longest_running_tv_series
+from Lidiia_6 import get_top_rated_horror_movies_by_year
 
 def main():
     spark = SparkSession.builder.appName("MySparkApp") \
@@ -39,6 +40,7 @@ def main():
     #genre_combination_popilarity(df_title_basic=df_title_basic, df_title_ratings= df_title_ratings)
     #get_3_top_rated_alive_principals_by_category(df_title_principals=df_title_principals, df_name_basics=df_name_basics, df_title_ratings=df_title_ratings)
     #get_longest_running_tv_series(df_title_episode=df_title_episode, df_title_basics=df_title_basic)
+    #get_top_rated_horror_movies_by_year(df_title_basic=df_title_basic, df_title_ratings=df_title_ratings)
 
     spark.stop()
 
